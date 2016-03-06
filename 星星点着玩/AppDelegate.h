1d7014d2f6ab2ecaa5b465cec1374d7a14da03ab
xiaotang <soul_chaoyue@163.com>
2016-03-06 20:17:32 +0800

    修改了动画时间

---
+++ 星星点着玩/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  星星点着玩
+//
+//  Created by 叶慧伟 on 16/3/5.
+//  Copyright © 2016年 叶慧伟. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
